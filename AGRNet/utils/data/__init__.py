8eddc8f66569db73041d630335df49ec0f0ef22d
rain4018 <2216335820@qq.com>
2025-09-25 19:00:01 +0800

    Delete AGRNet directory

--- AGRNet/utils/data/__init__.py
+++
@@ -1,9 +0,0 @@
-def get_dataset(dataset_name):
-    if dataset_name == 'cornell':
-        from .cornell_data import CornellDataset
-        return CornellDataset
-    elif dataset_name == 'cbrgd':
-        from .cbrgd_data import CBRGDDataset
-        return CBRGDDataset
-    else:
-        raise NotImplementedError('Dataset Type {} is Not implemented'.format(dataset_name))
